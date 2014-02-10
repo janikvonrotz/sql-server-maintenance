1cdfd1c9b4b03f1faa8bb33035687e463f5cbc5b
Janik von Rotz <login@janikvonrotz.ch>
2014-02-10 09:34:41 +0100

    test

--- Update-Repo.ps1
+++
@@ -1,3 +0,0 @@
-Install-PPApp "SQL Server Maintenance Solution"
-
-Copy-Item -Path "H:\SkyDrive\Shared\Projects\GitHub\Powershell-Profile\scripts\GitHubGist\ScheduleSQL Server Maintenance Solution Agent Jobs\*" -Destination "H:\SkyDrive\Shared\Projects\GitHub\SQL-Server-Maintenance\Schedule SQL Server Maintenance Solution Agent Jobs" -Filter "*.sql"
